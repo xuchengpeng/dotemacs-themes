18eb24088bc949273f71c7205d9d24dc6cba9133
xuchengpeng <330476629@qq.com>
2019-12-21 10:55:13 +0800

    dotemacs-solarized-dark: Fix helm-selection

--- themes/dotemacs-solarized-dark-theme.el
+++ themes/dotemacs-solarized-dark-theme.el
@@ -112,6 +112,11 @@ determine the exact padding."
 
    ((line-number &override) :foreground base4)
    ((line-number-current-line &override) :foreground fg)
+   
+   (helm-selection :inherit 'bold
+                   :background selection
+                   :distant-foreground bg
+                   :extend t)
 
    (font-lock-comment-face
     :foreground comments
