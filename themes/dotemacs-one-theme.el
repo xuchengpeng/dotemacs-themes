3cbab3cc365fb3316623e88406832a422c701ef2
xuchengpeng <330476629@qq.com>
2020-02-05 15:48:20 +0800

    Set a foreground for modelines

--- themes/dotemacs-one-theme.el
+++ themes/dotemacs-one-theme.el
@@ -91,7 +91,7 @@ determine the exact padding."
     (when dotemacs-one-padded-modeline
       (if (integerp dotemacs-one-padded-modeline) dotemacs-one-padded-modeline 4)))
 
-   (modeline-fg     nil)
+   (modeline-fg     fg)
    (modeline-fg-alt base5)
 
    (modeline-bg
